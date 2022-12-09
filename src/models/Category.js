967e90733ead74297245eb59873151f8f923bf1c
Brenno-Lucas <brennolucas8@gmail.com>
2022-12-09 09:52:07 -0300

    Requisito 07

---
+++ src/models/Category.js
@@ -0,0 +1,18 @@
+module.exports = (sequelize, DataTypes) => {
+  const Category = sequelize.define(
+    'Category',
+    {
+      id: {
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER,
+      },
+      name: DataTypes.STRING,
+    },
+    {
+      timestamps: false,
+      underscored: true,
+    }
+  );
+  return Category;
+};
